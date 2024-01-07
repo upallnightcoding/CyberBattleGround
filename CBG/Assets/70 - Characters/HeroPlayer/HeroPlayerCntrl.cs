6f3018138cbe00d064684fa03c3fb6932cb85c67
upallnightcoding <upallnightcoding@gmail.com>
2024-01-07 01:01:45 -0500

    Implement initial twin stick code

--- CBG/Assets/70 - Characters/HeroPlayer/HeroPlayerCntrl.cs
+++ CBG/Assets/70 - Characters/HeroPlayer/HeroPlayerCntrl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,25 @@ using UnityEngine.InputSystem;
 
 public class HeroPlayerCntrl : MonoBehaviour
 {
+    [SerializeField] private float playerSpeed = 5.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float controllerDeadZone = 0.1f;
+    [SerializeField] private float gamePadRotateSmoothing = 1000.0f;
+
+    [SerializeField] bool isGamePad;
+
+    private CharacterController charCntrl;
+
+    private Vector2 movement;
+    private Vector2 aim;
+
+    private Vector3 playerVelocity;
+
+    private void Awake()
+    {
+        charCntrl = GetComponent<CharacterController>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +34,37 @@ public class HeroPlayerCntrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleAim();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 move = new Vector3(movement.x, 0.0f, movement.y);
+        charCntrl.Move(move * playerSpeed * Time.deltaTime);
+    }
+
+    private void HandleAim()
+    {
+        //aim = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(aim);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+
+        if (groundPlane.Raycast(ray, out distance))
+        {
+            Vector3 point = ray.GetPoint(distance);
+            Vector3 target = new Vector3(point.x, transform.position.y, point.z);
+            transform.LookAt(target);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Debug.Log($"Mouse Move: {context.ReadValue<Vector2>()}");
+            movement = context.ReadValue<Vector2>();
+            Debug.Log($"Mouse Move: {movement}");
         }
     }
 
@@ -29,7 +72,8 @@ public class HeroPlayerCntrl : MonoBehaviour
     {
         if (context.performed)
         {
-            Debug.Log($"Aiming: {context.ReadValue<Vector2>()}");
+            aim = context.ReadValue<Vector2>();
+            Debug.Log($"Aiming: {aim}");
         }
     }
 }
