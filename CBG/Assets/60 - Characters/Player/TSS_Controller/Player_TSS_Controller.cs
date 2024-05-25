871cee78a4ce4af66e33b70f4a2e41e09027251b
upallnightcoding <upallnightcoding@gmail.com>
2024-05-25 02:40:50 -0400

    Twin Stick Keyboard Controller

--- CBG/Assets/60 - Characters/Player/TSS_Controller/Player_TSS_Controller.cs
+++ CBG/Assets/60 - Characters/Player/TSS_Controller/Player_TSS_Controller.cs
@@ -12,20 +12,160 @@ namespace TTS_Controller
         private Vector3 v3_aim;
         private float rotation_speed = 1000.0f;
 
+        private Vector2 leftControl, rightControl;
+
+        private Vector3 preTargetPoint = Vector3.zero;
+
         // Start is called before the first frame update
         void Start()
         {
             animator = GetComponent<Animator>();
         }
 
-        // Update is called once per frame
-        void Update()
+        void FixedUpdate()
+        {
+            //Update_TSS_Controller();
+            Update_TSS_KeyBoard();
+        }
+
+        void Update_TSS_KeyBoard()
+        {
+            //Vector2 rightControl = Gamepad.current.rightStick.value;
+
+            Vector3 camera_rotation = new Vector3(Camera.main.transform.forward.x, 0.0f, Camera.main.transform.forward.z);
+
+            v3_movement = new Vector3(leftControl.x, 0.0f, leftControl.y);
+            float movement_amount = Mathf.Abs(v3_movement.x) + Mathf.Abs(v3_movement.z);
+
+            //v3_aim = new Vector3(rightControl.x, 0.0f, rightControl.y);
+            //float aim_amount = Mathf.Abs(v3_aim.x) + Mathf.Abs(v3_aim.z);
+
+            //Vector2 targetPoint = Mouse.current.position.ReadValue();
+            Vector2 targetPoint = rightControl;
+            Debug.Log($"Target Point: {targetPoint}");
+            Vector3 hitPoint = Vector3.zero;
+            float aim_amount = 0.0f;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(targetPoint), out RaycastHit hit, 100.0f))
+            {
+                hitPoint = hit.point;
+                v3_aim = hitPoint - transform.position;
+                aim_amount = Vector3.Distance(hitPoint, preTargetPoint);
+                Debug.Log($"hitPoint/preTargetPoint: {hitPoint}/{preTargetPoint}");
+            }
+
+            if (aim_amount > 0.0)
+            {
+                Quaternion target_rotation = Quaternion.LookRotation(Quaternion.LookRotation(camera_rotation) * v3_aim);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, rotation_speed * Time.deltaTime);
+
+                Vector3 walk_aim_direction = Quaternion.LookRotation(new Vector3(-v3_aim.x, 0.0f, v3_aim.z)) * v3_movement;
+                animator.SetFloat("aim_x", walk_aim_direction.x, 0.15f, Time.deltaTime);
+                animator.SetFloat("aim_y", walk_aim_direction.z, 0.15f, Time.deltaTime);
+
+                preTargetPoint = hitPoint;
+            }
+            else
+            {
+                if (movement_amount > 0)
+                {
+                    Quaternion target_rotation = Quaternion.LookRotation(Quaternion.LookRotation(camera_rotation) * v3_movement);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, rotation_speed * Time.deltaTime);
+                }
+            }
+
+            if (aim_amount > 0)
+            {
+            }
+
+            Debug.Log($"Aim Amount/Aim Amount: {v3_aim}/{aim_amount}");
+            animator.SetFloat("movement_amount", movement_amount);
+            animator.SetBool("is_aiming", aim_amount > 0.0 ? true : false);
+            //animator.SetBool("is_aiming", true);
+
+        }
+
+        void Update_TSS_Cntrl()
+        {
+            Vector2 rightControl = Gamepad.current.rightStick.value;
+
+            Vector3 camera_rotation = new Vector3(Camera.main.transform.forward.x, 0.0f, Camera.main.transform.forward.z);
+
+            v3_movement = new Vector3(leftControl.x, 0.0f, leftControl.y);
+            v3_aim = new Vector3(rightControl.x, 0.0f, rightControl.y);
+
+            float movement_amount = Mathf.Abs(v3_movement.x) + Mathf.Abs(v3_movement.z);
+            float aim_amount = Mathf.Abs(v3_aim.x) + Mathf.Abs(v3_aim.z);
+
+            if (aim_amount > 0.3)
+            {
+                Quaternion target_rotation = Quaternion.LookRotation(Quaternion.LookRotation(camera_rotation) * v3_aim);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, rotation_speed * Time.deltaTime);
+
+                Vector3 walk_aim_direction = Quaternion.LookRotation(new Vector3(-v3_aim.x, 0.0f, v3_aim.z)) * v3_movement;
+                animator.SetFloat("aim_x", walk_aim_direction.x, 0.15f, Time.deltaTime);
+                animator.SetFloat("aim_y", walk_aim_direction.z, 0.15f, Time.deltaTime);
+            }
+            else
+            {
+                if (movement_amount > 0)
+                {
+                    Quaternion target_rotation = Quaternion.LookRotation(Quaternion.LookRotation(camera_rotation) * v3_movement);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, rotation_speed * Time.deltaTime);
+                }
+            }
+
+            if(aim_amount > 0)
+            {
+                Debug.Log($"Aim Amount/Aim Amount: {v3_aim}/{aim_amount}");
+            }
+
+            animator.SetFloat("movement_amount", movement_amount);
+            animator.SetBool("is_aiming", aim_amount > 0.3 ? true : false);
+
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                //Debug.Log($"OnMove Performed ... {context.ReadValue<Vector2>()}");
+                leftControl = context.ReadValue<Vector2>();
+            }
+
+            if (context.canceled)
+            {
+                leftControl = Vector2.zero;
+            }
+        }
+
+        public void OnAim(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                //Debug.Log($"OnAim Performed ... {context.ReadValue<Vector2>()}");
+                rightControl = context.ReadValue<Vector2>();
+            }
+        }
+
+        private void xxOnAnimatorMove()
+        {
+            Vector3 velocity = animator.deltaPosition;
+
+            //charCntrl.Move(velocity);
+        }
+
+        // ==================================================================
+        void Update_TSS_Controller()
         {
             var controller = Gamepad.current;
             if (controller == null) return;
 
             v3_movement = new Vector3(controller.leftStick.x.value, 0.0f, controller.leftStick.y.value);
             v3_aim = new Vector3(controller.rightStick.x.value, 0.0f, controller.rightStick.y.value);
+
+            Debug.Log($"Left Stick: {controller.leftStick.x.value}/{controller.leftStick.y.value}");
+            Debug.Log($"Right Stick: {controller.rightStick.x.value}/{controller.rightStick.y.value}");
 
             float movement_amount = Mathf.Abs(v3_movement.x) + Mathf.Abs(v3_movement.z);
             float aim_amount = Mathf.Abs(v3_aim.x) + Mathf.Abs(v3_aim.z);
---
+++ CBG/Assets/60 - Characters/Player/TSS_Keyboard/Player_TSS_Keyboard.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player_TSS_Keyboard
+{
+    public class Player_TSS_Keyboard : MonoBehaviour
+    {
+        private float speed = 4.0f;
+
+        private Vector2 leftControl, rightControl;
+
+        private CharacterController charCntrl;
+
+        private Vector3 lookPos;
+
+        private Animator animator;
+
+        private Transform cam;
+        private Vector3 camForward, move;
+        private float forwardAmount, turnAmount;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            charCntrl = GetComponent<CharacterController>();
+            animator = GetComponent<Animator>();
+
+            cam = Camera.main.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(rightControl);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100))
+            {
+                lookPos = hit.point;
+            }
+
+            Vector3 lookDir = lookPos - transform.position;
+            lookDir.y = 0.0f;
+
+            transform.LookAt(transform.position + lookDir, Vector3.up);
+
+            float horizontal = leftControl.x;
+            float vertical = leftControl.y;
+
+            camForward = Vector3.Scale(cam.up, new Vector3(1, 0, 1)).normalized;
+            move = vertical * camForward + horizontal * cam.right;
+            move.Normalize();
+
+            Vector3 localMove = transform.InverseTransformDirection(move);
+            turnAmount = localMove.x;
+            forwardAmount = localMove.z;
+
+            animator.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+            animator.SetFloat("Turn", turnAmount, 0.1f, Time.deltaTime);
+
+            Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+            charCntrl.Move(movement * speed * Time.deltaTime);
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                leftControl = context.ReadValue<Vector2>();
+            }
+
+            if (context.canceled)
+            {
+                leftControl = Vector2.zero;
+            }
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                rightControl = context.ReadValue<Vector2>();
+            }
+        }
+    }
+}
+
+
